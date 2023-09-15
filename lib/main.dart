fbb444ea7a6e85d1f04ff1ecaa0a2b2184af0b33
Matheas T <tmatheas@gmail.com>
2023-09-15 08:17:01 +0530

    Version 1

---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+import 'package:expense_tracker/expenses.dart';
+import 'package:flutter/services.dart';
+
+var kColorSchema = ColorScheme.fromSeed(
+  seedColor: Color.fromARGB(255, 181, 59, 65),
+);
+
+var kDarkColorSchema = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: const Color.fromARGB(255, 5, 99, 125),
+);
+
+void main() {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // SystemChrome.setPreferredOrientations(
+  //   [DeviceOrientation.portraitUp],
+  // ).then((fn) => {
+  runApp(
+    MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(
+        useMaterial3: true,
+        colorScheme: kDarkColorSchema,
+        cardTheme: const CardTheme().copyWith(
+          color: kDarkColorSchema.secondaryContainer,
+          margin: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 8,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kDarkColorSchema.primaryContainer,
+            foregroundColor: kColorSchema.onPrimaryContainer,
+          ),
+        ),
+      ),
+      theme: ThemeData().copyWith(
+        useMaterial3: true,
+        colorScheme: kColorSchema,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorSchema.onPrimaryContainer,
+          foregroundColor: kColorSchema.primaryContainer,
+        ),
+        cardTheme: const CardTheme().copyWith(
+          color: kColorSchema.secondaryContainer,
+          margin: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 8,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorSchema.primaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+              titleLarge: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: kColorSchema.onSecondaryContainer,
+                fontSize: 16,
+              ),
+            ),
+      ),
+      themeMode: ThemeMode.system,
+      home: const Expenses(),
+    ),
+  );
+  // });
+}
---
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -0,0 +1,27 @@
+import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/expenses_list/expense_item.dart';
+import 'package:flutter/material.dart';
+
+class ExpensesList extends StatelessWidget {
+  const ExpensesList(
+      {super.key, required this.expenses, required this.onRemoveExpense});
+
+  final void Function(Expense expense) onRemoveExpense;
+
+  final List<Expense> expenses;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: expenses.length,
+      itemBuilder: (ctx, index) => Dismissible(
+          onDismissed: (direction) {
+            onRemoveExpense(expenses[index]);
+          },
+          key: ValueKey(
+            expenses[index],
+          ),
+          child: ExpenseItem(expense: expenses[index])),
+    );
+  }
+}
